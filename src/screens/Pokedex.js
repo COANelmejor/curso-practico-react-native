659d08feddd64913d24656820e34dc7fb47026f8
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-05 17:35:16 -0600

    Add useEffect hook to load pokemons from API

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,24 @@
 import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getPokemonsApi } from '../api/pokemon'
 
 export default function PokedexScreen() {
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons()
+    })();
+  }, [])
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi()
+      console.log(JSON.stringify(response, null, 2))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
