d476c4acb12b504d5ee8acf674da3c0511132ec2
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-05 17:57:06 -0600

    Refactor getPokemonsApi and add getPokemonDetailsByUrlApi functions

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,18 +1,30 @@
 import { API_URL_POKEAPI } from "../utils/constants";
 
+const defaultOptions = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
 export async function getPokemonsApi () {
   try {
     const url = `${API_URL_POKEAPI}/pokemon?limit=20&offset=0`
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    const response = await fetch(url, defaultOptions)
     const result = await response.json()
     return result
   } catch (error) {
     throw error
   }
 
+}
+
+export async function getPokemonDetailsByUrlApi (url) {
+  try {
+    const response = await fetch(url, defaultOptions)
+    const result = await response.json()
+    return result
+  } catch (error) {
+    throw error
+  }
 }
