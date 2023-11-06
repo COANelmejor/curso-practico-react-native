c4f33b43061e9510cec882da9de40e26690be5d5
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-06 01:33:33 -0600

    Add Pokemon details to PokemonScreen component

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { getPokemonDetailsApi } from '../api/pokemon';
 
-export default function PokemonScreen() {
+export default function PokemonScreen(props) {
+  const {
+    navigation,
+    route: {
+      params
+    }
+  } = props;
+  const [pokemon, setPokemon] = useState({})
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id)
+        setPokemon(response)
+      } catch (error) {
+        console.error(error)
+        navigation.goBack();
+      }
+    })()
+  }, [params])
+
+  if (!pokemon) return null;
+
   return (
-    <View>
-      <Text>Estamos en un Pokemon</Text>
-    </View>
+    <SafeAreaView>
+      <Text>{pokemon.name}</Text>
+      <Text>{pokemon.id}</Text>
+      <Text>{pokemon.height}</Text>
+      <Text>{pokemon.weight}</Text>
+      <Text>{pokemon.base_experience}</Text>
+    </SafeAreaView>
   )
 }
