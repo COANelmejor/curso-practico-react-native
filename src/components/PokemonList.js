64c4d64c2c3efb689bc54fa25d4e83ca644dc75c
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-06 00:16:43 -0600

    Refactor PokemonList component Now accepts a loading state

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,42 +1,43 @@
-import { StyleSheet, FlatList, ActivityIndicator, Platform } from 'react-native'
-import React from 'react'
-import PokemonCard from './PokemonCard'
+import { ActivityIndicator, FlatList, Platform, StyleSheet } from 'react-native';
+import React from 'react';
+import PokemonCard from './PokemonCard';
 
-export default function PokemonList(props) {
-  const {
-    pokemons,
-    loadPokemons,
-    isNext
-  } = props
-
-  const loadMorePokemons = () => {
-    console.log("Cargando más pokemons");
+export default function PokemonList({ pokemons, loadPokemons, isNext, isLoading }) {
+  const loadMore = () => {
     loadPokemons();
-  }
+  };
+
   return (
     <FlatList
       data={pokemons}
       numColumns={2}
       showsVerticalScrollIndicator={false}
       keyExtractor={(pokemon) => String(pokemon.id)}
-      renderItem={({ item }) => (<PokemonCard pokemon={item} />)}
+      renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
-      onEndReached={isNext && loadMorePokemons}
-      onEndReachedThreshold={0.25}
+      onEndReached={!isLoading && isNext && loadMore}
+      onEndReachedThreshold={2}
       ListFooterComponent={
-        isNext && <ActivityIndicator style={styles.spinner} size="large" color="#000" />
+        isLoading &&
+        isNext && (
+          <ActivityIndicator
+            size="large"
+            style={styles.spinner}
+            color="#AEAEAE"
+          />
+        )
       }
     />
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
-    marginTop: Platform.OS === 'ios' ? 5 : 10,
+    marginTop: Platform.OS === 'android' ? 30 : 0,
   },
   spinner: {
     marginTop: 20,
-    marginBottom: Platform.OS === 'ios' ? 80 : 60,
-  }
-})
+    marginBottom: Platform.OS === 'android' ? 90 : 60,
+  },
+});
