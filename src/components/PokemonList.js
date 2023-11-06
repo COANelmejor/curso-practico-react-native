d1faf1a755e8c1135d42803bfc1584304127af1c
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-05 23:22:09 -0600

    Add infinite scroll and loading spinner to PokemonList component

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, FlatList } from 'react-native'
+import { StyleSheet, FlatList, ActivityIndicator, Platform } from 'react-native'
 import React from 'react'
 import PokemonCard from './PokemonCard'
 
 export default function PokemonList(props) {
   const {
-    pokemons
+    pokemons,
+    loadPokemons,
+    isNext
   } = props
+
+  const loadMorePokemons = () => {
+    console.log("Cargando más pokemons");
+    loadPokemons();
+  }
   return (
     <FlatList
       data={pokemons}
@@ -14,6 +21,11 @@ export default function PokemonList(props) {
       keyExtractor={(pokemon) => String(pokemon.id)}
       renderItem={({ item }) => (<PokemonCard pokemon={item} />)}
       contentContainerStyle={styles.flatListContentContainer}
+      onEndReached={isNext && loadMorePokemons}
+      onEndReachedThreshold={0.25}
+      ListFooterComponent={
+        isNext && <ActivityIndicator style={styles.spinner} size="large" color="#000" />
+      }
     />
   )
 }
@@ -21,5 +33,10 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
+    marginTop: Platform.OS === 'ios' ? 5 : 10,
+  },
+  spinner: {
+    marginTop: 20,
+    marginBottom: Platform.OS === 'ios' ? 80 : 60,
   }
 })
