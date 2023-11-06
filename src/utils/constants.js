6a817c71d64466737ba9605a58b60143aac60ba5
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-05 19:38:40 -0600

    Add Pokemon type colors to constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,22 @@
 export const API_URL_POKEAPI = 'https://pokeapi.co/api/v2'
+
+export const POKEMON_TYPE_COLORS = {
+  bug: "#A8B820",
+  dark: "#705848",
+  dragon: "#7038F8",
+  electric: "#FFCE4B",
+  fairy: "#EE99AC",
+  fighting: "#C03028",
+  fire: "#FA6C6C",
+  flying: "#A890F0",
+  ghost: "#705898",
+  grass: "#48CFB2",
+  ground: "#E0C068",
+  ice: "#98D8D8",
+  normal: "#A8A878",
+  poison: "#A040A0",
+  psychic: "#F85888",
+  rock: "#B8A038",
+  steel: "#B8B8D0",
+  water: "#6890F0",
+};
