da368e5276a1b16821fa7d3a8258ac12fc017a75
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-07 00:39:16 -0600

    Add Type component for displaying Pokemon types

---
+++ src/components/Pokemon/Type.js
@@ -0,0 +1,46 @@
+import { StyleSheet, View, Text } from 'react-native'
+import getColorByPokemonType from '../../utils/getColorByPokemonType';
+
+export default function Type(props) {
+  const { types } = props;
+
+
+  console.log(JSON.stringify(types, null, 2))
+  return (
+    <View style={styles.content}>
+      {types.map((item, index) => {
+
+        return (
+          <View key={index} style={{
+            ...styles.pill,
+            backgroundColor: getColorByPokemonType(item.type.name)
+          }}>
+            <Text style={styles.textPill}>{item.type.name}</Text>
+          </View>
+        )
+      })}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  content: {
+    marginTop: 50,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pill: {
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 20,
+    marginHorizontal: 10,
+  },
+  textPill: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  }
+})
