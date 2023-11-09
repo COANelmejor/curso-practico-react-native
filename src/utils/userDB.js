389316331c2225d41aedf4d7d03cb4866bb59b18
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-08 21:25:18 -0600

    Add userDB.js with user and userDetail objects

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "username",
+  password: "password",
+}
+
+export const userDetail = {
+  username: "username",
+  firstName: "Christian",
+  lastName: "Arana",
+  email: "coanelmejor@gmail.com",
+}
