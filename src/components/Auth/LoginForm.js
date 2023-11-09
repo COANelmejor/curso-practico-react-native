e8e817f6234ad22e7513bcbf5ca7396ad5cfaa7b
Christian O. Arana Navas (COANelmejor) <COANelmejor@gmail.com>
2023-11-08 21:45:08 -0600

    Add login form with styled components

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Keyboard
+} from 'react-native';
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        placeholder='Usuario'
+        style={styles.input}
+        autoCapitalize='none'
+      />
+      <TextInput
+        placeholder='Contraseña'
+        style={styles.input}
+        autoCapitalize='none'
+        secureTextEntry={true}
+      />
+      <Button
+        title='Iniciar Sesión'
+        onPress={() => console.log('Iniciar Sesión')}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  }
+})
